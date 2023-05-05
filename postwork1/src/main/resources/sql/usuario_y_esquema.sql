e92e828a003baffac153e6d403cfe55334ec1996
Victor Sanchez <soporteaplicaciones@innovasport.com>
2023-05-04 23:29:23 -0600

    correccion de sql

--- postwork1/src/main/resources/sql/usuario_y_esquema.sql
+++ postwork1/src/main/resources/sql/usuario_y_esquema.sql
@@ -1,6 +1,8 @@
 create schema if not exists `javase2_bedu_pw`;
 
-grant all on javase2_bedu_pw.* to 'pwadmin'@'localhost' identified by 'ADMIN_PW_PWD';
+create user if not exists 'pwadmin'@'localhost' identified by 'ADMIN_PW_PWD';
+
+grant all on javase2_bedu_pw.* to 'pwadmin'@'localhost';
 
 
 flush privileges;
